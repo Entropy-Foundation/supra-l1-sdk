35324cfe8f7c8a7753fa89f76743b2b3d117b7f5
vpanchal-supra <v.panchal@supraoracles.com>
2024-12-09 17:41:42 +0530

    Changed build strategy

--- tsup.config.js
+++ tsup.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
   splitting: false,
   sourcemap: true,
-  target: "es2018",
+  target: "es2020",
 });
