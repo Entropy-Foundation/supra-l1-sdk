3abb01bb6e14b805181480cf270fe053712d9158
vpanchal-supra <v.panchal@supraoracles.com>
2024-12-09 20:49:15 +0530

    minor change related to build

--- tsup.config.js
+++ tsup.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  minify: true,
   splitting: false,
   sourcemap: true,
   target: "es2020",
