d45f0bab5b6b21f5ba1c6029a822f5f507c64c0a
vpanchal-supra <v.panchal@supraoracles.com>
2025-04-08 18:57:43 +0530

    Addressed issue-39 ad issue-38

--- src/utils.ts
+++ src/utils.ts
@@ -30,14 +30,12 @@ export const fromUint8ArrayToJSArray = (
 };
 
 export const normalizeAddress = (addressToNormalize: string): string => {
-  if (
-    addressToNormalize.length > 64 &&
-    addressToNormalize.slice(0, 2) === "0x"
-  ) {
-    addressToNormalize = addressToNormalize.slice(2);
-  }
-  if (addressToNormalize.length != 64) {
-    throw new Error("address length must be 64 or it's size must be 256 bits");
+  if (addressToNormalize.length != 66) {
+    if (addressToNormalize.slice(0, 2) === "0x") {
+      addressToNormalize = addressToNormalize.slice(2).padStart(64, "0");
+    } else {
+      addressToNormalize = addressToNormalize.padStart(64, "0");
+    }
   }
   return addressToNormalize;
 };
