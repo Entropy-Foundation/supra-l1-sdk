41cfe5e4d0810b991ec6cc834a0b106ee713dd32
vpanchal-supra <v.panchal@supraoracles.com>
2025-07-28 17:29:07 +0530

    Rollback min gas price

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const DEFAULT_RECORDS_ITEMS_COUNT = 15;
 // The `maximum_number_of_gas_units` amount at move layer is 2 million
 export const DEFAULT_MAX_GAS_UNITS = BigInt(500000);
 // The `min_price_per_gas_unit` amount at move layer is 100
-export const DEFAULT_GAS_PRICE = BigInt(5000);
+export const DEFAULT_GAS_PRICE = BigInt(100);
 export const DEFAULT_TX_EXPIRATION_DURATION = 300; // 5 Minutes
 export const MILLISECONDS_PER_SECOND = 1000;
 export const SUPRA_FRAMEWORK_ADDRESS =
